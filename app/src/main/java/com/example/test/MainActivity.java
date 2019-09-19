a1ea13a0397bf97ee1db1dd6d9c238e7b4eec825
Rajat Sangrame <rajatsangrame@gmail.com>
2019-09-19 14:22:45 +0530

    Master

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClick(View view) {
+
+        int i = 5;
+        Intent intent = new Intent(this, MyPhoneReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        assert alarmManager != null;
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (i * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm set in " + i + " seconds",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/test/MyPhoneReceiver.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyPhoneReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Don't panik but your time is up!!!!.",
+                Toast.LENGTH_LONG).show();
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        assert vibrator != null;
+        vibrator.vibrate(2000);
+    }
+}
